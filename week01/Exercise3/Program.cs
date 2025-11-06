4691e1f4ae681e2a9b705f3b139906db0ea2770d
maiselamadise <maisela.mad@gmail.com>
2025-11-06 09:22:58 +0200

    Commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+      Console.Write("What is the magic number? ");
+        string input = Console.ReadLine();
+        int magicNumber = int.Parse(input);
+
+        Console.Write("What is you guess? ");
+        string guessInput = Console.ReadLine();
+        int guess = int.Parse(guessInput);
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
